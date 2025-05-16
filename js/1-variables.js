fd0a8d1d0e994f40e0abedd478dc970bcc5a2c46
jeyson08 <jeyson.boursault@gmail.com>
2025-05-16 21:08:34 +0200

    1-bases 1-variables

---
+++ js/1-variables.js
@@ -0,0 +1,13 @@
+let nombre1;
+let nombre2;
+let nombre3;
+let prenom;
+
+nombre1 = 5;
+nombre2 = 30;
+nombre3 = nombre1 + nombre2;
+
+console.log(nombre1);
+console.log(nombre2);
+console.log(nombre3);
+console.log(prenom);
