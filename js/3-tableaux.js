9189f4fc78770a02875bfad35ae08a8c623335e2
jeyson08 <jeyson.boursault@gmail.com>
2025-05-19 08:01:11 +0200

    1-bases 3-tableaux

---
+++ js/3-tableaux.js
@@ -0,0 +1,14 @@
+let tabIndex = ["dupont", "jean", 30];
+let tabAssociatif = {
+  nom: "dupont",
+  prenom: "jean",
+  age: "30",
+};
+
+console.log(
+  `tabIndex : Je m'appelle ${tabIndex[0]} ${tabIndex[1]} et j'ai ${tabIndex[2]} ans`
+);
+
+console.log(
+  `tabAssociatif : Je m'appelle ${tabAssociatif.nom} ${tabAssociatif.prenom} et j'ai ${tabAssociatif.age} ans`
+);
